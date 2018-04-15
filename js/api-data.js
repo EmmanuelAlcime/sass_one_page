7eb897abd5edfbc85c6f3bf121f1e02461cfccd4
emmanuelalcime <emmanuelalcime54@gmail.com>
2018-04-15 00:10:58 -0400

    added code preview to div

--- js/api-data.js
+++ js/api-data.js
@@ -1,20 +1,40 @@
+/*
+* AUTHOR: Emmanuel Alcime
+* DATE: 04/14/2018
+* URI: emmanuelalcime.github.io/personal_page
+*/
 
-var i,
-    dataContainer = document.getElementsByClassName("boxes");
+
+
+var dataContainer = document.getElementsByClassName("boxes");
+var dataDiv = document.getElementsByClassName("info-data");
 var apiExample = {};
-apiExample._createBindings = function(dataContainer){
+apiExample._createBindings = function(dataContainer, dataDiv){
    var details =  dataContainer.getElementsByClassName("view-details")[0];
    var dataInfo = dataContainer.getElementsByClassName("info-data")[0];
+   var closeButton = dataDiv.getElementsByClassName("closeButton")[0];
    details.addEventListener("click",(e)=>{
-   	 var evt = e.target || window.event;
-   	 //var tar = e.target || e.srcElement;
-     
+   	   openCode(e, dataInfo);
+   });
+   closeButton.addEventListener("click",(e)=>{
+   	    closeBox(e, dataInfo);
    });
 };
+
 apiExample._init = function(){
-  for(i = 0; i < dataContainer.length; i++ ){
-	  this._createBindings(dataContainer[i]);
+  for(var i = 0; i < dataContainer.length; i++ ){
+	  this._createBindings(dataContainer[i],dataDiv[i]);
   }
 }; 
+apiExample._init();
+
+function openCode(e, dataInfo){
+  // check to see if any other div is open 
+  dataInfo.style.display = "block"; // code example into view 
+}
+
+function closeBox(e, dataInfo){
+   dataInfo.style.display = "none"; // code example into view 
+}// close a certain box when called
 
 
