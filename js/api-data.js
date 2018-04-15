5591f3dd4ee4263e8bc8fdcbfc996fca0758a5bd
emmanuelalcime <emmanuelalcime54@gmail.com>
2018-04-15 16:12:02 -0400

    added triangle indication mark

--- js/api-data.js
+++ js/api-data.js
@@ -13,11 +13,12 @@ apiExample._createBindings = function(dataContainer, dataDiv){
    var details =  dataContainer.getElementsByClassName("view-details")[0];
    var dataInfo = dataContainer.getElementsByClassName("info-data")[0];
    var closeButton = dataDiv.getElementsByClassName("closeButton")[0];
+   var triangle = dataContainer.getElementsByClassName("triangle")[0];
    details.addEventListener("click",(e)=>{
-   	   openCode(e, dataInfo);
+   	   openCode(e, dataInfo,triangle);
    });
    closeButton.addEventListener("click",(e)=>{
-   	    closeBox(e, dataInfo);
+   	    closeBox(e, dataInfo, triangle);
    });
 };
 apiExample._init = function(){
@@ -26,21 +27,27 @@ apiExample._init = function(){
   }
 }; 
 apiExample._init();
-function openCode(e, dataInfo){
+function openCode(e, dataInfo, triangle){
   // check to see if any other div is open 
   var infoWin = document.getElementsByClassName("info-data");
+  var tri = document.getElementsByClassName("triangle");
   for(var j = 0; j < infoWin.length; j++){
   	 if(infoWin[j].getAttribute("style")){
   	 	toggleDisplay(infoWin[j]); // close existing window
+      toggleDisplay(tri[j]);
   	 	infoWin[j].removeAttribute("style"); // remove style attribute from current open window
+      tri[j].removeAttribute("style");
   	 }
   }
   dataInfo.style.display = "block"; // open new window 
+  triangle.style.display = "block";
 }
-function closeBox(e, dataInfo){
+function closeBox(e, dataInfo, triangle){
    dataInfo.style.display = "none"; // code example into view 
+   triangle.style.display = "none";
    if(dataInfo.getAttribute("style")){
       dataInfo.removeAttribute("style"); // we need to clear the style attribute when we close the window.
+      triangle.removeAttribute("style");
    }
 }// close a certain box when called
 
