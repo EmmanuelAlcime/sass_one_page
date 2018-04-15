93157b4d4a0e94b8d62431854528d428aa3c48a5
emmanuelalcime <emmanuelalcime54@gmail.com>
2018-04-15 16:43:42 -0400

    completed contact form script

--- js/contact.form.js
+++ js/contact.form.js
@@ -8,6 +8,22 @@ var purpose = document.getElementById("purpose").value;
 
 document.getElementById("form_submit")
 .addEventListener("click",(e)=>{
+	var alertBanner = document.getElementById("alert");
     e.preventDefault();
-    //alert("working");
+    if (firstName === "" || lastName ==="" 
+	|| email === "" || url === "" || purpose === "" ) {
+        alertBanner.setAttribute("class","alert alert-danger reminder");
+        alertBanner.innerHTML = "Fields left blank"; 
+        alertBanner.style.display = "block";
+        setTimeout(function(){
+         alertBanner.style.display = "none";
+        },6000);        
+    }else{
+       alertBanner.setAttribute("class","alert alert-success reminder");
+        alertBanner.innerHTML = "Request Sent"; 
+        alertBanner.style.display = "block";
+        setTimeout(function(){
+         alertBanner.style.display = "none";
+        },6000);      
+    }
 });
