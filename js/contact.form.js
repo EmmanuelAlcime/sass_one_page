54fb32c556f2185508be2d89de9b2ad6151cb895
emmanuelalcime <emmanuelalcime54@gmail.com>
2018-05-25 10:09:03 -0400

    started work on front end contact form script

--- js/contact.form.js
+++ js/contact.form.js
@@ -1,29 +1,21 @@
-var firstName = document.getElementById("first_name").value;
-var lastName = document.getElementById("last_name").value;
-var email = document.getElementById("email").value;
-var url = document.getElementById("url").value;
-var purpose = document.getElementById("purpose").value;
+;(function(){
+  'use strict';
+
+var firstName = document.getElementById("first_name").value; // first name
+
+var lastName = document.getElementById("last_name").value; // last name
+var email = document.getElementById("email").value; // email address
+var url = document.getElementById("url").value;  // website url
+var purpose = document.getElementById("purpose").value;  // request purpose 
 
 
 
 document.getElementById("form_submit")
 .addEventListener("click",(e)=>{
-	var alertBanner = document.getElementById("alert");
-    e.preventDefault();
-    if (firstName === "" || lastName ==="" 
-	|| email === "" || url === "" || purpose === "" ) {
-        alertBanner.setAttribute("class","alert alert-danger reminder");
-        alertBanner.innerHTML = "Fields left blank"; 
-        alertBanner.style.display = "block";
-        setTimeout(function(){
-         alertBanner.style.display = "none";
-        },6000);        
-    }else{
-       alertBanner.setAttribute("class","alert alert-success reminder");
-        alertBanner.innerHTML = "Request Sent"; 
-        alertBanner.style.display = "block";
-        setTimeout(function(){
-         alertBanner.style.display = "none";
-        },6000);      
-    }
-});
+        var alertBanner = document.getElementById("alert");
+         e.preventDefault();
+        // validate inputs from front end script.
+
+    
+  });
+})();
